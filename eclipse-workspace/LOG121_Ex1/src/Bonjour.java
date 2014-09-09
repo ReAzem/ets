9af5e5605037a6929ae51a52cf49ece496d1d606
aviau <alexandre.viau@savoirfairelinux.com>
2014-09-09 14:36:50 -0400

    Initial commit

---
+++ eclipse-workspace/LOG121_Ex1/src/Bonjour.java
@@ -0,0 +1,32 @@
+/****************************************
+Cours:              LOG121
+Session:            Automne 2014
+Groupe:             03
+Projet:             Exercice 1
+Étudiant(e)(s)      Viau, Alexandre
+Code(s) perm.:      VIAA08029409
+Chargé de cours:    Dominic St-Jacques
+Chargés de labo:    Alvine Boaye et Jean-Nichola Blanchet
+Nom du ficher:      Bonjour.java
+Date créé:          2014-09-09
+Date dern. modif.:  2014-09-09
+*****************************************
+Historique des modificaitons
+*****************************************
+2014-09-09          Version initiale
+*****************************************/
+
+/**
+ * @author Alexandre Viau
+ */
+public class Bonjour {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Bonjour tout le monde !");
+
+	}
+
+}
