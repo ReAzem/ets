06d5a557e2b5759486b9249db93253e8d8ba00e4
aviau <alexandre.viau@savoirfairelinux.com>
2014-09-09 16:31:04 -0400

    Ajout du squelette Forme

---
+++ eclipse-workspace/LOG121_Lab1/src/Forme.java
@@ -0,0 +1,22 @@
+/****************************************
+Cours:              LOG121
+Session:            Automne 2014
+Groupe:             03
+Projet:             Exercice 1
+Étudiant(e)(s)      Viau, Alexandre
+Code(s) perm.:      VIAA08029409
+Chargé de cours:    Dominic St-Jacques
+Chargés de labo:    Alvine Boaye et Jean-Nichola Blanchet
+Nom du ficher:      Bonjour.java
+Date créé:          2014-09-09
+Date dern. modif.:  2014-09-09
+*****************************************
+Historique des modificaitons
+*****************************************
+2014-09-09          Version initiale
+*****************************************/
+
+
+public class Forme {
+
+}
