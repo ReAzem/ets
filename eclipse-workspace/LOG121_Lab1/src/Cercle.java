cf0aa8545630750257cb70bd3deb6ab2fb5fdd0f
aviau <alexandre.viau@savoirfairelinux.com>
2014-09-23 14:18:13 -0400

    Specify private

--- eclipse-workspace/LOG121_Lab1/src/Cercle.java
+++ eclipse-workspace/LOG121_Lab1/src/Cercle.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 
 
 public class Cercle extends Forme {
-	int centreX, centreY, rayon;
+	private int centreX, centreY, rayon;
 
 	public Cercle(int numero_sequence, int centreX, int centreY, int rayon){
 		super(numero_sequence);
